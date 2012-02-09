04ee05aabc2bc0ff94addcffec5c2aefb2d3721b
Mat Sadler <mat@redstudio.co.uk>
2012-02-09 11:36:42 +0000

    [#24625439] added lowres images of stands

--- application/views/mainpage/main.php
+++ application/views/mainpage/main.php
@@ -1,5 +1,15 @@
- <div id="parallax" >
-     <div style="width:1800px; height: 800px; background: url('http://webdev.stephband.info/jparallax/images/parallax_target/target_blue.png') transparent;">test</div>
-      <div style="width:2000px; height: 900px; background: url('http://webdev.stephband.info/jparallax/images/parallax_target/target_green.png') transparent;">baa</div>
-      <div style="width:2300px; height: 1000px; background: url('http://webdev.stephband.info/jparallax/images/parallax_target/target_red.png') transparent;">baa</div>
+<div id="loadInfo">
+	<div id="statusBar"><div id="status"></div></div>
+	<p id="textStatus"></p>
+</div> 
+
+<div id="parallax" >
+  
+      <div id="parallax_background">baa</div>
+      <div id="parallax_foreground">
+          
+          <span id="preloader" style="position:absolute; top:200px; left:200px; width:200px; height:200px; border:1px solid #bbbbbb; color: #fff">...</span>
+           <span style="position:absolute; top:900px; left:700px; width:200px; height:200px; border:1px solid #bbbbbb; color: #000">another stand here</span>
+          
+      </div>
   </div>
--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
+$(function(){
+			$('#loadInfo').hide();
+			$('a.button').click(function(){
+				$('#loadInfo').slideDown();
+				$(this).parent().after('<h2>Images loaded into DOM from CSS:</h2><ul id="loadedImgs"></ul>')
+				var loadedImgs = $.preloadCssImages({statusTextEl: '#textStatus', statusBarEl: '#status'});
+				for(var i = 0; i<loadedImgs.length; i++){
+					$('#loadedImgs').append('<li><img src=\"'+loadedImgs[i].src+'\" />: '+loadedImgs[i].src+'</li>');
+				}
+				//resize parent iframe to contain new images
+				$(window.parent.document).find('iframe:eq(0)').height('110em');
+$('#loadInfo').hide();
+			});
+
+		});
+
 $(document).ready(function(){  
  
-        $('#parallax div').parallax(); 
+    $('#parallax div').parallax({
+            
+        frameDuration: 40,
+        ytravel:0,
+        xtravel:1
+            
+    }); 
  
-  });
+});
