c6199304c3d53ed0da64b1613cc8a30c15bf245e
Mat Sadler <mat@redstudio.co.uk>
2012-08-28 14:39:44 +0000

    added icons to front page

--- application/views/slideshow/main/picture.php
+++ application/views/slideshow/main/picture.php
@@ -1,29 +1,29 @@
 <div class="one-third column colourContainer">
-	<div class="boxesFront green1"><a href="<?=base_url()?>portable"><img src="<?=base_url()?>images/icons/facebook_32.png"/></a></div>
+	<div class="boxesFront green1"><a href="<?=base_url()?>portable"><img height="130px" src="<?=base_url()?>images/icons/portablewhite.png"/></a></div>
 <h4>Portable</h4>
 </div>
 
 <div class="one-third column colourContainer">
-	<div class="boxesFront green2"></div>
+	<div class="boxesFront green2"><a href="<?=base_url()?>modular"><img height="130px" src="<?=base_url()?>images/icons/modularwhite.png"/></a></div>
 <h4>Modular</h4>
 </div>
 
 <div class="one-third column colourContainer">
-	<div class="boxesFront lightblue"></div>
+	<div class="boxesFront lightblue"><a href="<?=base_url()?>roadshows"><img height="130px" src="<?=base_url()?>images/icons/roadshowswhite.png"/></a></div>
 	<h4>Roadshows</h4>
 </div>
 
 <div class="one-third column colourContainer">
-	<div class="boxesFront pink1"></div>
+	<div class="boxesFront pink1"><a href="<?=base_url()?>portable"><img height="130px" src="<?=base_url()?>images/icons/custombuildwhite.png"/></a></div>
 	<h4>Custom Build</h4>
 </div>
 
 <div class="one-third column colourContainer">
-	<div class="boxesFront orange1"></div>
+	<div class="boxesFront orange1"><a href="<?=base_url()?>portable"><img height="130px" src="<?=base_url()?>images/icons/outdoorwhite.png"/></a></div>
 	<h4>Outdoor Exhibitions</h4>
 </div>
 
 <div class="one-third column colourContainer">
-	<div class="boxesFront purple1"></div>
+	<div class="boxesFront purple1"><a href="<?=base_url()?>portable"><img height="130px" src="<?=base_url()?>images/icons/access360white.png"/></a></div>
 	<h4>Case Studies</h4>
 </div>
